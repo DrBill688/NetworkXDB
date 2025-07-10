728bccb22d0b8214d2b5897f8df49ff694f31a97
DrBill688 <drbill688@gmail.com>
2025-07-10 19:03:40 -0400

    Copyright

--- NetworkXDB.py
+++ NetworkXDB.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jul  6 12:07:17 2025
+NetworkXDB is a small python library exposes a RDBMS persisted knowledge graph using NetworkX
+    Copyright (C) 2025  William N. Roney, ScD (drbill688@gmail.com)
 
-@author: wrone
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import networkx as nx
